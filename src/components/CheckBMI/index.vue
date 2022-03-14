1a7d21713c8c7613e8a94061bc0d37c7feed185c
Attaulla9 <attaulla.az@gmail.com>
2022-03-15 01:20:31 +0530

    handle time and Date

--- src/components/CheckBMI/index.vue
+++ src/components/CheckBMI/index.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="md:w-2/3 mx-auto">
+  <div class="md:w-2/4 mx-auto space-y-4">
+    <div class="heading text-center mt-4 space-y-2">
+      <h1 class="text-2xl font-semibold">
+        Book Your <span class="text-red-400">Free</span> Consultation
+      </h1>
+      <p>( 30 minutes consultation )</p>
+      <p>Please Select Date & Time</p>
+    </div>
     <div v-if="loading">
-      <div
-        class="
-          md:w-1/2
-          grid grid-cols-4
-          text-center
-          gap-4
-          px-2
-          py-2
-          mt-4
-          mx-auto
-        "
-      >
+      <div class="md:w-1/2 grid grid-cols-4 text-center py-2 mx-auto">
         <label
           v-for="(item, value, index) in AllSlots"
           :key="index"
@@ -21,12 +17,16 @@
             radio-group
             px-4
             py-2
-            m-2
             shadow-md
             rounded
             text-sky-800
             border-2 border-sky-600
             mx-auto
+            transition
+            ease-in
+            delay-150
+            duration-200
+            focus:
           "
           :class="checked == value ? 'bg-sky-100' : 'bg-white'"
         >
@@ -34,7 +34,7 @@
             type="radio"
             :id="'data' + index"
             name="data-slot"
-            class="py-2 px-4 rounded shadow cursor-pointer hidden"
+            class="cursor-pointer hidden"
             :value="value"
             v-model="checked"
           />
@@ -43,30 +43,17 @@
           </p>
         </label>
       </div>
-      <p class="text-center text-sky-800 font-bold space-x-2"><span class="inline-block">{{ checked }}</span><span class="text-sky-700 inline-block"> {{selectedTime}}</span></p>
+      <p class="text-center text-sky-800 font-bold space-x-2 my-4">
+        <span class="inline-block">{{ checked }}</span
+        ><span class="text-sky-700 inline-block"> {{ selectedTime }}</span>
+      </p>
 
-      <div
-        class="
-          mt-6
-          grid
-          md:grid-cols-6
-          grid-cols-3
-          gap-3
-          px-2
-          mx-auto
-          transform
-          transition-all
-          duration-700
-          ease-in
-        "
-      >
+      <div class="grid md:grid-cols-6 grid-cols-3 gap-3 px-2 mx-auto">
         <button
           class="
-            py-2
-            px-4
+            p-2
             rounded
-            shadow-lg
-            border-2 border-sky-300
+            drop-shadow-md
             font-medium
             focus:bg-sky-800 focus:text-white focus:border-white
           "
@@ -75,36 +62,41 @@
           @click="getTime(slot.slot)"
           :class="
             slot.colour == 'yellow'
-              ? ['bg-white', 'text-sky-600']
-              : ['bg-yellow-200', 'text-sky-800']
+              ? ['bg-yellow-200', 'text-sky-600']
+              : ['bg-white', 'text-sky-800']
           "
         >
           {{ slot.slot }}
         </button>
       </div>
 
-      <div class="px-2 mb-20 mt-10 flex flex-row justify-center space-x-2">
+      <div
+        class="px-2 flex flex-row justify-center space-x-2 mt-6 mb-28 md:mb-6"
+      >
         <p class="flex justify-center items-center space-x-2">
-          <span class="h-5 w-5 bg-yellow-300"></span><span>Fast Filling</span>
+          <span class="h-5 w-5 bg-yellow-200"></span><span>Filling Fast</span>
         </p>
         <p class="flex justify-center items-center space-x-2">
-          <span class="h-5 w-5 bg-red-300"></span><span>Fast Filling</span>
+          <span class="h-5 w-5 bg-red-500"></span><span>Filled</span>
         </p>
         <p class="flex justify-center items-center space-x-2">
           <span class="h-5 w-5 border-2 border-sky-800"></span
-          ><span>Fast Filling</span>
+          ><span>Available</span>
         </p>
       </div>
       <div
         class="
           fixed
+          md:static
           bottom-0
           left-0
           w-full
           p-1
           border-t-4
+          md:border-0
           bg-white
           border-sky-100
+          md:shadow-none
           shadow-lg
           text-center
         "
@@ -127,7 +119,30 @@
         left-1/2
       "
     >
-      <p>Fetching...</p>
+      <svg
+        role="status"
+        class="
+          mr-2
+          w-16
+          h-16
+          text-gray-200
+          animate-spin
+          dark:text-gray-600
+          fill-blue-400
+        "
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
     </div>
   </div>
 </template>
@@ -144,11 +159,10 @@ export default {
       checked: "",
     };
   },
-  watch:{
-    checked(){
-      this.selectedTime=''
-      
-    }
+  watch: {
+    checked() {
+      this.selectedTime = "";
+    },
   },
   computed: {
     dateFormate(value) {
--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="py-2 bg-slate-200 shadow-md w-full flex justify-center items-center bg-gradient-to-r from-sky-100 to-blue-100  ">
+  <header class="py-2 shadow-md w-full flex justify-center items-center  ">
     <img src="@/assets/logo.png" width="140" height="140" alt="Possible Logo" />
   </header>
 </template>
